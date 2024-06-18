c1a5cd640bb8d94848bb769e4cb9dfd2b9c0d9a3
Cristian <fake@ema.il>
2024-06-18 13:51:51 +0200

    5-Implementar distintos endings y cada uno devuelve el calculo indicado

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/hola', (req, res) => {
-  res.send('Hello World!')
+app.get('/suma/:sumando1&:sumando2', (req, res) => {
+  var suma1 = Number(req.params.sumando1);
+  var suma2 = Number(req.params.sumando2);
+  resultado = suma1 + suma2;
+  res.send(resultado)
+})
+
+app.get('/multiplicacion/:multiplicando1&:multiplicando2', (req, res) => {
+  var mult1 = Number(req.params.multiplicando1);
+  var mult2 = Number(req.params.multiplicando2);
+  resultado = mult1 * mult2;
+  res.send(resultado)
+})
+
+app.get('/lista-de-1/:a', (req, res) => {
+  var a = Number(req.params.a);
+  for (i = 0; i <= a; i++) {
+      var lista = [];
+
+      resultado = lista.push(i)
+  }
+  res.send(resultado)
+
 })
 
 app.listen(port, () => {
